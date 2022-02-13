0636809ab2102c7207f6ea92a0d57d5cd6fecb2b
xiaoming <wen.xiaoming@outlook.com>
2022-02-13 10:29:50 +0800

    hot-fix commit-1

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -2,7 +2,5 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git!");
-        System.out.println("hot fix------!");
-
     }
 }
